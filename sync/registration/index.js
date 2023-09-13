25433bccd94d4c12cb93c42c5a9e985045872b5c
Marc Duiker <marcduiker@users.noreply.github.com>
2023-09-13 15:49:46 +0200

    Remove get
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- sync/registration/index.js
+++ sync/registration/index.js
@@ -5,10 +5,6 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from NodeJS!');
-});
-
 app.post('/register', async (req, res) => {
   const registrationMessage = `Registration received for ${req.body.name}`
   console.log(registrationMessage);
