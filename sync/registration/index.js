736e21e75f74e18e01e427e9aa86787d2e9687f3
Marc Duiker <marcduiker@users.noreply.github.com>
2023-09-13 15:47:33 +0200

    Sync example is working
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- sync/registration/index.js
+++ sync/registration/index.js
@@ -1,3 +1,6 @@
+const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
+const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3501";
+
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -6,10 +9,21 @@ app.get('/', (req, res) => {
   res.send('Hello from NodeJS!');
 });
 
-app.post('/register', (req, res) => {
-    const registrationMessage = `Registration received for ${req.body.name}`
-    console.log(registrationMessage);
-    res.send(registrationMessage);
+app.post('/register', async (req, res) => {
+  const registrationMessage = `Registration received for ${req.body.name}`
+  console.log(registrationMessage);
+  const paymentData = { "email": req.body.email, "cost": req.body.cost };
+  const response = await fetch(`${DAPR_HOST}:${DAPR_HTTP_PORT}/pay`, {
+    method: "POST",
+    body: JSON.stringify(paymentData),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      "dapr-app-id": "payment"}
+  });
+  if (!response.ok) {
+    console.error(`Error calling payment service: ${response.status} ${response.statusText}`);
+  }
+  res.send(registrationMessage);
 });
 
 const PORT = process.env.PORT || 5501;
