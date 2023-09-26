0443edd1f13201adcae6ac80f56d11c175a75cc6
Marc Duiker <marcduiker@users.noreply.github.com>
2023-09-26 11:17:19 +0200

    Add deadletter handling
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- async/payment/index.js
+++ async/payment/index.js
@@ -8,17 +8,28 @@ app.use(bodyParser.json({ type: 'application/*+json' }));
 
 app.get('/dapr/subscribe', (_req, res) => {
   res.json([
-      { 
-          pubsubname: PUBSUB_COMPONENT,
-          topic: PUBSUB_TOPIC,
-          route: "pay"
-      }
+    { 
+      pubsubname: PUBSUB_COMPONENT,
+      topic: PUBSUB_TOPIC,
+      route: "pay"
+    }
   ]);
 });
 
 app.post('/pay', (req, res) => {
-    console.log("Payment received:", req.body.data);
-    res.sendStatus(200);
+  console.log("Payment received:", req.body.data);
+  const payload = JSON.parse(req.body.data);
+  if (payload.email === "") {
+      res.sendStatus(400);
+  }
+  else {
+    res.sendStatus(204);
+  }
+});
+
+app.post('/invalid', (req, res) => {
+  console.log("Invalid message received:", req.body.data);
+  res.sendStatus(204);
 });
 
 const PORT = process.env.PORT || 5512;
